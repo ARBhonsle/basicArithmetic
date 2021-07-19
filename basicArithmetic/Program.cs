d233d505aa332cd68d0b99d8550d3a0757e4be51
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-19 13:14:06 +0530

    [aditi] Add: Arithmetic operations(add,subtract and multiply) added

--- basicArithmetic/Program.cs
+++ basicArithmetic/Program.cs
@@ -6,7 +6,19 @@ namespace basicArithmetic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculator Application");
+            Console.WriteLine("Adding, subtracting and multiplying numbers");
+            Console.WriteLine("Enter number a :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number b :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Sum of given numbers: " + a + " and " + b + " is " + c);
+            c = a - b;
+            Console.WriteLine("Difference between given numbers: " + a + " and " + b + " is " + c);
+            c = a * b;
+            Console.WriteLine("Multiplication of given numbers: " + a + " and " + b + " is " + c);
+            Console.WriteLine("Exiting Application ....");
         }
     }
 }
